ad02403d1aaf42eefe7bf10e44e82e701d752577
Jack Maguire <github.29x6f@simplelogin.com>
2023-03-10 18:52:26 +0000

    working multi-file

--- src/expandablebitvec.zig
+++ src/expandablebitvec.zig
@@ -1,7 +1,13 @@
-pub fn ebv() type {
-    return struct {
-        pub fn abc() i32 {
-            return -1;
-        }
-    };
-}
+pub const ExpandableBitVec = struct {
+    backing: [100]u32,
+
+    pub fn new () ExpandableBitVec {
+        return ExpandableBitVec {
+            .backing = [_]u32{0} ** 100
+        };
+    }
+
+    pub fn abc (self: *ExpandableBitVec) i32 {
+        return self.backing.len;
+    }
+};
